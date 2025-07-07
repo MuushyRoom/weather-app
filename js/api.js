b212343ee9f7cd0e46ed1412ed5dc9d30a6c1025
MuushyRoom <ronancuaresma10@gmail.com>
2025-07-07 23:57:40 +0800

    API IMPLEMENTED SUCCESS 40% WORK DONE!

---
+++ js/api.js
@@ -0,0 +1,50 @@
+// GEOLOCATION API
+const options = {
+    maximumAge:0,
+    enableHighAccuracy: false,
+    timeout:1500,
+};
+
+//GET COORDS IF SUCCESS
+const success = (pos) => {
+const coords = pos.coords
+const lon = coords.longitude;
+const lat = coords.latitude;
+
+//
+getWeather(lat,lon)
+
+}
+
+const error = (err) => {
+console.log(err)
+};
+
+//GET USER PERMISSION FOR LOCATION
+navigator.geolocation.getCurrentPosition(success, error, options)
+    console.log()
+
+
+// CREATE AN ASYNC FUNCTION FOR GETTING DATA
+async function getWeather(lat,lon){
+
+    try{
+    const api_key = "37e5574b2579cb49aa9987e7ece20b6f";
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`
+
+         const response = await fetch(url)
+         const data = await response.json()
+         console.log("🚀 ~ getWeather ~ data:", data)
+      
+        displayWeather(data)
+       
+    }catch(error){
+        console.log(error)
+    }finally{
+        console.log("get weather executed")
+    }
+  
+   
+}
+
+
---
+++ js/app.js
@@ -0,0 +1,59 @@
+const place = document.querySelector('.place-container')
+const currentDay = document.querySelector('.current-day')
+const currentTemp = document.querySelector('.primary-temp')
+const feelsLikeTemp = document.querySelector('.feels-like-temp')
+const maxTemp = document.querySelector('.max-temp-container')
+console.log("🚀 ~ maxTemp:", maxTemp)
+const minTemp = document.querySelector('.min-temp-container')
+console.log("🚀 ~ minTemp:", minTemp)
+// console.log("🚀 ~ currentTemp:", currentTemp)
+// console.log("🚀 ~ currentDay:", currentDay)
+// console.log("🚀 ~ place:", place)
+
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const d = new Date();
+let day = weekday[d.getDay()];
+async function displayWeather(data){
+const weatherInfo = await data
+
+const city = weatherInfo.name
+const country = weatherInfo.sys.country
+
+
+// GETTING CURRENT TEMP
+let tempK = weatherInfo.main.temp
+let tempC = tempK - 273.15;
+let currentTempC = tempC.toFixed(0)
+
+
+// GETTING MAX TEMP
+let tempKMax = weatherInfo.main.temp_max
+let tempCMax = tempKMax - 273.15;
+let tempMax = tempCMax.toFixed(0)
+
+// GETTING MIN TEMP
+let tempKMin = weatherInfo.main.temp_min
+let tempCMin = tempKMin - 273.15;
+let tempMin = tempCMin.toFixed(0)
+
+
+// GETTING FEELS LIKE TEMP
+let tempKFeelsLike = weatherInfo.main.feels_like
+let tempCFeelsLike = tempKFeelsLike - 273.15;
+let tempFeelsLike = tempCFeelsLike.toFixed(0)
+
+maxTemp.innerHTML=`MAX.${tempMax}°C`;
+minTemp.innerHTML=`MIN.${tempMin}°C`;
+feelsLikeTemp.innerHTML=`Feels like ${tempFeelsLike}°C`;
+currentDay.innerText = day;
+place.innerText =`${city}, ${country}`;
+currentTemp.innerText=`${currentTempC}°C`;
+
+
+displayIcon()
+}
+
+
+function displayIcon(){
+    
+}
